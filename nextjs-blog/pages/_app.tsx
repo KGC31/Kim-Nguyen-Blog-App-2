3c97fbf1973475a9faf7d6e33098771bb6d82eb0
KGC31 <kimnguyenblc@gmail.com>
2024-05-22 16:14:50 +0700

    styles: custom cursor

--- nextjs-blog/pages/_app.tsx
+++ nextjs-blog/pages/_app.tsx
@@ -2,8 +2,10 @@ import type { AppProps } from 'next/app'
 import type { Metadata } from 'next'
 import { useRouter } from 'next/router'
 import { motion, AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
 
-import Header from '../components/header'
+import Header from '../components/navbar'
+import Cursor from '../components/cursor'
 
 import '../styles/global.css'
 
@@ -16,16 +18,20 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
   return (
     <AnimatePresence mode='wait'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <motion.div key={router.pathname}> 
         <Header></Header>
+        <Cursor></Cursor>
         <Component {...pageProps} />
 
-        <motion.div className='slide-in' initial={{ scaleY: 0 }}
+        <motion.div className='slide-in z-50' initial={{ scaleY: 0 }}
                                           animate={{scaleY: 0 }}
                                           exit={{ scaleY: 1 }}
                                           transition={{duration: 1, ease : [0.22, 1, 0.36, 1] }}
         ></motion.div>
-        <motion.div className='slide-out' initial={{ scaleY: 1 }}
+        <motion.div className='slide-out z-50' initial={{ scaleY: 1 }}
                                           animate={{scaleY: 0 }}
                                           exit={{ scaleY: 0 }}
                                           transition={{duration: 1, ease : [0.22, 1, 0.36, 1] }}
