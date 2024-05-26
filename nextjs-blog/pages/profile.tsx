9c3484892fe3dfad236b5aa51b634dd43069518c
KGC31 <kimnguyenblc@gmail.com>
2024-05-26 21:45:02 +0700

    feat: profile with user information

--- nextjs-blog/pages/profile.tsx
+++ nextjs-blog/pages/profile.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { getCsrfToken } from '../utils/csrf';
+import styles from '../styles/profile.module.css';
 
 export default function UserProfile() {
     const [userData, setUserData] = useState(null);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
+    useEffect(() => {       
         const user_id = localStorage.getItem('user_id');
         const jwt_token = localStorage.getItem('access_token');
 
@@ -31,30 +33,74 @@ export default function UserProfile() {
 
                 const data = await response.json();
                 setUserData(data);
+                console.log(data);
             } catch (error) {
                 console.error('Fetch error:', error);
                 setError('Failed to fetch user data');
+            } finally {
+                setLoading(false);
             }
         };
-
+        
         if (user_id && jwt_token) {
             fetchUserData();
         } else {
             setError('User ID or token is missing');
+            setLoading(false);
         }
     }, []);
 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
     return (
-        <div>
+        <>
             {userData && (
                 <div>
-                    <p>Username: {userData.username}</p>
-                    <p>Email: {userData.email}</p>
-                    <p>First Name: {userData.first_name}</p>
-                    <p>Last Name: {userData.last_name}</p>
+                    <div className={styles.container}>
+                    </div>
+                    <div className='-translate-y-20'>
+                        <div className='w-screen px-20 py-2'>
+                            <div className='border border-[#fff] rounded-xl flex place-items-center p-4 bg-white'>
+                                <img 
+                                    src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541" 
+                                    height={64} 
+                                    width={64} 
+                                    alt="Logo" 
+                                    className='border rounded-xl' 
+                                />
+                                <h1 className='text-2xl ms-4'>Welcome {userData.username}!</h1>
+                            </div>
+                        </div>
+                        <div className='mt-5 w-screen px-20'>
+                            <div className='grid grid-rows-2 grid-cols-2 bg-white p-10 gap-10 border rounded-xl'>
+                                <div>
+                                    <h1 className='text-xl'>Email</h1>
+                                    <input className='border-s-2 border-black ps-0.5' type="text" placeholder={userData.email} />
+                                </div>
+                                <div>
+                                    <h1 className='text-xl'>Username</h1>
+                                    <input className='border-s-2 border-black ps-0.5' type="text" placeholder={userData.username} />
+                                </div>
+                                <div>
+                                    <h1 className='text-xl'>First name</h1>
+                                    <input className='border-s-2 border-black ps-0.5' type="text" placeholder={userData.first_name} />
+                                </div>
+                                <div>
+                                    <h1 className='text-lg'>Last name</h1>
+                                    <input className='border-s-2 border-black ps-0.5' type="text" placeholder={userData.last_name} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             )}
-            {error && <p>{error}</p>}
-        </div>
+        </>
     );
 }
