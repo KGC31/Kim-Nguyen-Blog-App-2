2757b47b355852d4f616368870ec98b985f418fa
KGC31 <kimnguyenblc@gmail.com>
2024-06-06 15:25:14 +0700

    build: update setting for deployment

--- server/server/settings.py
+++ server/server/settings.py
@@ -29,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yn-vtb^^f2=oubuq1()h=euk)@=huqq491znmp-1ys+g@0r!r6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True'
+DEBUG = 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', ".vercel.app", ".now.sh"]
 
 
 # Application definition
