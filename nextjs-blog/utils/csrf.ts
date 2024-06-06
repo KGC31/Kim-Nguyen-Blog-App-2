e3d6ce8ae0659106c8ab1afb204e3c20b44b5fc4
KGC31 <kimnguyenblc@gmail.com>
2024-06-06 15:50:14 +0700

    test: test backend url

--- nextjs-blog/utils/csrf.ts
+++ nextjs-blog/utils/csrf.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getCsrfToken() {
     try {
-        const response = await axios.get('http://localhost:8000/api/csrf-token/', {
+        const response = await axios.get('https://kim-nguyen-blog-app-server-26dt91tgs-kgc31s-projects.vercel.app/api/csrf-token/', {
             withCredentials: true,  // Ensure cookies are included in the request
             headers: {
                 'Content-Type': 'application/json',
